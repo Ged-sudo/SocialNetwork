c3b8f7ca7e15394ac260349f83b3604c065d1c85
Ged-sudo <gafter921@yandex.ru>
2022-02-22 19:03:02 +0300

    edit some interface 2

--- SocialNetwork/AppDelegate.swift
+++ SocialNetwork/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let navigationBarAppearace = UINavigationBar.appearance()
+            navigationBarAppearace.barTintColor = .white //your desired color
+            navigationBarAppearace.tintColor = .white //your button etc color
+            navigationBarAppearace.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         return true
     }
 
--- SocialNetwork/ViewController.swift
+++ SocialNetwork/ViewController.swift
@@ -8,24 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = postTableView.dequeueReusableCell(withIdentifier: "postcell",  for: indexPath) as! PostCell
-        return cell
-    }
+    
     
 
     @IBOutlet weak var postTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         postTableView.delegate = self
         postTableView.dataSource = self
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = postTableView.dequeueReusableCell(withIdentifier: "postcell",  for: indexPath) as! PostCell
+        
+        cell.textInPost.text = "Some text in post"
+        
+        cell.layer.borderWidth = 5
+        cell.layer.borderColor = UIColor.systemBrown.cgColor
+        return cell
+    }
 
 }
 
